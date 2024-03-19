282de3762830038ff2e55b398ed6a536f9e1f931
matteussbt@gmail.com <matteussbt@gmail.com>
2024-03-19 14:01:29 -0300

    Ajusta metodos INSS, IRRF e máscara para formulario

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const formulario = document.getElementById('formulario');
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
-
     
     nav.classList.toggle('active');
     const active = nav.classList.contains('active');
@@ -17,6 +16,43 @@ function toggleMenu(event) {
     }
 }
 
+function calcularInss() {
+    let salarioBruto = parseFloat(document.getElementById('salario-bruto').value);
+    salarioBruto *= 1000;
+    
+    if (salarioBruto <= 1412) {
+        return salarioBruto * 0.075;
+    } else if (salarioBruto > 1412 && salarioBruto <= 2666.68) {
+        return (salarioBruto * 0.09) - 21.18;
+    } else if (salarioBruto > 2666.68 && salarioBruto <= 4000.03) {
+        return (salarioBruto * 0.12) - 101.18;
+    } else if (salarioBruto > 4000.03 && salarioBruto < 7786.03) {
+        return (salarioBruto * 0.14) - 181.18;
+    } else {
+        return 908.86; 
+    }
+
+}
+
+function calcularIrrf() {
+    let salarioBruto = parseFloat(document.getElementById('salario-bruto').value);
+    salarioBruto *= 1000;
+    const dependentes = document.getElementById('dependentes').value;
+    const deducaoDependenteIrrf = dependentes * 189.59;
+    const baseIrrf = salarioBruto - calcularInss() - deducaoDependenteIrrf;
+
+    if (baseIrrf <= 2259.20) {
+        return baseIrrf * 0;
+    } else if (baseIrrf > 2259.20 && baseIrrf <= 2826.65) {
+        return (baseIrrf * 0.075) - 169.44;
+    } else if (baseIrrf > 2826.65 && baseIrrf <= 3751.05) {
+        return (baseIrrf * 0.15) - 381.44;
+    } else if (baseIrrf > 3751.05 && baseIrrf < 4664.68) {
+        return (baseIrrf * 0.225) - 662.77;
+    } else {
+        return (baseIrrf * 0.275) - 896;       
+    }
+}
 
 formulario.addEventListener('submit', event => {
     event.preventDefault();
@@ -24,74 +60,36 @@ formulario.addEventListener('submit', event => {
     // CAPTAÇÃO DE DADOS DO FORMULÁRIO
     const salarioBruto = document.getElementById('salario-bruto').value;
     const descontos = document.getElementById('descontos').value;
-    const dependentes = document.getElementById('dependentes').value;
-
-    //CÁLCULO INSS
-    let inss = 0;
-    let aliquotaInss = 0;
-    
-    if (salarioBruto <= 1412) {
-        inss = salarioBruto * 0.075;
-        aliquotaInss = (inss / salarioBruto) * 100;
-    } else if (salarioBruto > 1412 && salarioBruto <= 2666.68) {
-        inss = (salarioBruto * 0.09) - 21.18;
-        aliquotaInss = (inss / salarioBruto) * 100;
-    } else if (salarioBruto > 2666.68 && salarioBruto <= 4000.03) {
-        inss = (salarioBruto * 0.12) - 101.18;
-        aliquotaInss = (inss / salarioBruto) * 100;
-    } else if (salarioBruto > 4000.03 && salarioBruto < 7786.03) {
-        inss = (salarioBruto * 0.14) - 181.18;
-        aliquotaInss = (inss / salarioBruto) * 100;
-    } else {
-        inss = 908.86;
-        aliquotaInss = (inss / salarioBruto) * 100;     
-    }
-
-    // CÁLCULO IRRF
-    let irrf = 0;
-    let aliquotaIrrf = 0;
-    const deducaoDependente = dependentes * 189.59;
-    const baseIrrf = salarioBruto - inss - deducaoDependente;
-
-    if (baseIrrf <= 2259.20) {
-        irrf = baseIrrf * 0;
-        aliquotaIrrf = 0;
-    } else if (baseIrrf > 2259.20 && baseIrrf <= 2826.65) {
-        irrf = (baseIrrf * 0.075) - 169.44;
-        aliquotaIrrf = (irrf / salarioBruto) * 100;
-    } else if (baseIrrf > 2826.65 && baseIrrf <= 3751.05) {
-        irrf = (baseIrrf * 0.15) - 381.44;
-        aliquotaIrrf = (irrf / salarioBruto) * 100;
-    } else if (baseIrrf > 3751.05 && baseIrrf < 4664.68) {
-        irrf = (baseIrrf * 0.225) - 662.77;
-        aliquotaIrrf = (irrf / salarioBruto) * 100;
-    } else {
-        irrf = (baseIrrf * 0.275) - 896;
-        aliquotaIrrf = (irrf / salarioBruto) * 100;
-        
-    }
-    
+     
     document.getElementById('resultado').classList.remove('hidden');
 
-    // CÁLCULO TOTAL DE DESCONTOS
-    const totalDescontos = parseFloat(descontos) + inss + irrf;
-    const valorLiquido = salarioBruto - totalDescontos;
-
+    // CÁLCULOS: TOTAL DE DESCONTOS, VALOR LÍQUIDO, ALÍQUOTA REAL INSS E IRRF
+    const aliquotaInss = (calcularInss() / parseFloat(salarioBruto)) / 10;
+    const aliquotaIrrf = (calcularIrrf() / parseFloat(salarioBruto)) / 10;
+    const totalDescontos = parseFloat(descontos) + calcularInss() + calcularIrrf();
+    function calcularLiquido() {
+        let salarioBruto = parseFloat(document.getElementById('salario-bruto').value);
+        salarioBruto *= 1000;
+    
+        return salarioBruto - totalDescontos;
+    }
 
     // INCLUSÃO DE INFORMAÇÕES NA TABELA
     document.getElementById('salario').textContent = salarioBruto;
-    document.getElementById('tabela-descontos').textContent = descontos.replace('.', ',');
-    document.getElementById('total-proventos').textContent = salarioBruto.replace('.', ',');
 
-    document.getElementById('valor-inss').textContent = inss.toFixed(2).replace('.', ',');
-    document.getElementById('aliquota-inss').textContent = aliquotaInss.toFixed(2).replace('.', ',') + '%';
-
-    document.getElementById('valor-irrf').textContent = irrf.toFixed(2).replace('.', ',');
-    document.getElementById('aliquota-irrf').textContent = aliquotaIrrf.toFixed(2).replace('.', ',') + '%';
-    document.getElementById('total-descontos').textContent = totalDescontos.toFixed(2).replace('.', ',');
-    document.getElementById('liquido').textContent = valorLiquido.toFixed(2).replace('.', ',');
-
+    document.getElementById('tabela-descontos').textContent = descontos;
     
+    document.getElementById('aliquota-inss').textContent = aliquotaInss.toFixed(2).replace('.', ',') + '%';
+    document.getElementById('valor-inss').textContent = calcularInss().toFixed(2).replace('.', ',');
+    
+    document.getElementById('aliquota-irrf').textContent = aliquotaIrrf.toFixed(2).replace('.', ',') + '%';
+    document.getElementById('valor-irrf').textContent = calcularIrrf().toFixed(2).replace('.', ',');
+    
+    document.getElementById('total-proventos').textContent = salarioBruto;
+    document.getElementById('total-descontos').textContent = totalDescontos.toFixed(2).replace('.', ',');
+    
+    document.getElementById('liquido').textContent = calcularLiquido().toFixed(2).replace('.', ',');
+
 }
 )
 
